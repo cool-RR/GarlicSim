83f31d5144e75d77c8d688c7fed30791ecd9d15d
Ram Rachum <cool-rr@cool-rr.com>
2011-02-11 15:06:30 -0500

    -

--- garlicsim/garlicsim/general_misc/delete_this.py
+++ garlicsim/garlicsim/general_misc/delete_this.py
@@ -2,21 +2,21 @@
 
 
 
-def divisorGen(n):
-    factors = list(factorGenerator(n))
-    nfactors = len(factors)
-    f = [0] * nfactors
-    while True:
-        yield reduce(lambda x, y: x*y, [factors[x][0]**f[x] for x in range(nfactors)], 1)
-        i = 0
-        while True:
-            f[i] += 1
-            if f[i] <= factors[i][1]:
-                break
-            f[i] = 0
-            i += 1
-            if i >= nfactors:
-                return
+#def divisorGen(n):
+    #factors = list(factorGenerator(n))
+    #nfactors = len(factors)
+    #f = [0] * nfactors
+    #while True:
+        #yield reduce(lambda x, y: x*y, [factors[x][0]**f[x] for x in range(nfactors)], 1)
+        #i = 0
+        #while True:
+            #f[i] += 1
+            #if f[i] <= factors[i][1]:
+                #break
+            #f[i] = 0
+            #i += 1
+            #if i >= nfactors:
+                #return
 
 import operator
 import itertools
@@ -45,28 +45,33 @@ def get_clean_counter(counter):
         del clean_counter[empty_key]
     return clean_counter
 
+
 def get_contained_counters(counter):
     clean_counter = get_clean_counter(counter)
     keys, values = zip(*clean_counter.items())
-    value_combinations = itertools.product((xrange(value) for value in values))
+    value_combinations = itertools.product(
+        (xrange(value + 1) for value in values)
+    )
     contained_counters = []
     for value_combination in value_combinations:
         contained_counters.append(Counter(zip(keys, value_combination)))
     return contained_counters
-    
-        
-    
 
-def alt_get_divisors(x):
+
+def get_divisors(x):
+    prime_divisors_counter = Counter(get_prime_divisors(x))
+    contained_prime_divisors_counters = \
+        get_contained_counters(prime_divisors_counter)
+    divisors = []
+    for contained_prime_divisors_counter in contained_prime_divisors_counters:
+        divisors.append(product(contained_prime_divisors_counter.elements()))
+    return divisors
     
-    
-            
-                
 
 from garlicsim.general_misc import cute_profile
         
         
-def get_divisors(x):
+def alt_get_divisors(x):
     return [i for i in xrange(1, x) if (x % i == 0)]
 
 def is_perfect(x):
