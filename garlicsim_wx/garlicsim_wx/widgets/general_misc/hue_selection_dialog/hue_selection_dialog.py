73cddd90c56eb0ec64f313248feb8a71ccc718bc
Ram Rachum <cool-rr@cool-rr.com>
2011-06-01 22:56:02 -0400

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
@@ -29,14 +29,10 @@ import wx
 from garlicsim_wx.widgets.general_misc.cute_panel import CutePanel
 
 
-class Textual(CutePanel):
+class Textual(wx.Panel):
     '''Display (and allow modifying) the hue as a number 0-359.'''
     def __init__(self, hue_selection_dialog):
         wx.Panel.__init__(self, parent=hue_selection_dialog, size=(75, 100))
-        self.set_good_background_color()
-        self.SetHelpText(
-            u'Set the hue in angles (0%s-359%s).' % (unichr(176), unichr(176))
-        )
         
         self.hue_selection_dialog = hue_selection_dialog
         #self.hue = hue_selection_dialog.hue
@@ -119,80 +115,17 @@ class Comparer(CutePanel):
                                     else wx.SUNKEN_BORDER)
         wx.Panel.__init__(self, parent=hue_selection_dialog, size=(75, 90),
                           style=style)
-        self.SetDoubleBuffered(True)
-        self.SetHelpText('The current hue is shown next to the old hue for '
-                         'comparison. To change back to the old hue, click on '
-                         'it.')
-        self.hue_selection_dialog = hue_selection_dialog
-        assert isinstance(self.hue_selection_dialog, HueSelectionDialog)
-        #self.hue = hue_selection_dialog.hue
-        #self.old_hls = hue_selection_dialog.old_hls
-        #self.old_hue = hue_selection_dialog.old_hue
-        #self.old_color = wx_tools.colors.hls_to_wx_color(self.old_hls)
-        #self.negative_old_color = wx_tools.colors.invert_wx_color(self.color)
-        #self.old_brush = wx.Brush(self.old_color)
         self._transparent_pen = \
             wx.Pen(wx.Colour(0, 0, 0), width=0, style=wx.TRANSPARENT)
-        #self._calculate()
         
         self.SetCursor(wx.StockCursor(wx.CURSOR_BULLSEYE))
         
-        #self.Bind(wx.EVT_PAINT, self._on_paint)
         self.Bind(wx.EVT_LEFT_DOWN, self._on_mouse_left_down)
         self.Bind(wx.EVT_SET_FOCUS, self._on_set_focus)
         self.Bind(wx.EVT_KILL_FOCUS, self._on_kill_focus)
         self.Bind(wx.EVT_CHAR, self._on_char)
         
     
-    #@property
-    #def color(self):
-        #return wx_tools.colors.hls_to_wx_color(
-            #(self.hue,
-             #self.hue_selection_dialog.lightness,
-             #self.hue_selection_dialog.saturation)
-        #)
-        
-        
-    #def _calculate(self):
-        #'''Create a brush for showing the new hue.'''
-        #self.brush = wx.Brush(self.color)
-        
-        
-    #def update(self):
-        #'''If hue changed, show new hue.'''
-        #if self.hue != self.hue_selection_dialog.hue:
-            #self.hue = self.hue_selection_dialog.hue
-            #self._calculate()
-            #self.Refresh()
-            
-    
-    #def change_to_old_hue(self):
-        #self.hue_selection_dialog.setter(self.old_hue)
-
-            
-    #def _on_paint(self, event):
-        #width, height = self.GetClientSize()
-        #dc = wx.BufferedPaintDC(self)
-        #graphics_context = wx.GraphicsContext.Create(dc)
-        #assert isinstance(graphics_context, wx.GraphicsContext)
-
-        #dc.SetPen(self._transparent_pen)
-        
-        #dc.SetBrush(self.brush)
-        #dc.DrawRectangle(0, 0, width, (height // 2))
-                    
-        #dc.SetBrush(self.old_brush)
-        #dc.DrawRectangle(0, (height // 2), width, (height // 2) + 1)
-        
-        #if self.has_focus():
-            #graphics_context.SetPen(
-                #wx_tools.drawing_tools.pens.get_focus_pen(
-                    #self.negative_old_color
-                #)
-            #)
-            #graphics_context.SetBrush(self.old_brush)
-            #graphics_context.DrawRectangle(3, (height // 2) + 3,
-                                           #width - 6, (height // 2) - 6)
                 
     
     def _on_mouse_left_down(self, event):
