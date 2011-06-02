28c552d051fac128358941d0f56088ac81e9fae7
Ram Rachum <cool-rr@cool-rr.com>
2011-06-01 22:59:32 -0400

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
@@ -1,41 +1,17 @@
 # Copyright 2009-2011 Ram Rachum.
 # This program is distributed under the LGPL2.1 license.
 
-'''
-Defines the `HueSelectionDialog` class.
 
-See its documentation for more details.
-'''
-
-# todo: should have validation in `Textual`, currently can enter words
-
-from __future__ import division
 from __future__ import with_statement
 
 import wx
 
-from garlicsim.general_misc import freezers
 from garlicsim_wx.general_misc import wx_tools
-from garlicsim_wx.widgets.general_misc.cute_panel import CutePanel
-from garlicsim.general_misc.context_managers import ReentrantContextManager
-
-
-
-
-        
-
-import wx
-
-from garlicsim_wx.widgets.general_misc.cute_panel import CutePanel
 
 
 class Textual(wx.Panel):
-    '''Display (and allow modifying) the hue as a number 0-359.'''
     def __init__(self, hue_selection_dialog):
         wx.Panel.__init__(self, parent=hue_selection_dialog, size=(75, 100))
-        
-        self.hue_selection_dialog = hue_selection_dialog
-        #self.hue = hue_selection_dialog.hue
         
         self.main_v_sizer = wx.BoxSizer(wx.VERTICAL)
         
@@ -51,64 +27,14 @@ class Textual(wx.Panel):
         self.spin_ctrl = wx.SpinCtrl(self, min=0, max=359,
                                      initial=77,
                                      size=(70, -1), style=wx.SP_WRAP)
-        #if wx_tools.is_mac:
-            #self.spin_ctrl.SetValue(ratio_to_round_degrees(self.hue))
         
         self.h_sizer.Add(self.spin_ctrl, 0)
         
-        self.degree_static_text = wx.StaticText(self, label=unichr(176))
-        
-        self.h_sizer.Add(self.degree_static_text, 0)
-        
         self.SetSizerAndFit(self.main_v_sizer)
         
-        self.Bind(wx.EVT_SPINCTRL, self._on_spin, source=self.spin_ctrl)
-        self.Bind(wx.EVT_TEXT, self._on_text, source=self.spin_ctrl)
-        #self.Bind(wx.EVT_KILL_FOCUS, self._on_kill_focue)
         
-        
-    value_freezer = freezers.FreezerProperty()
-                    
-        
-    def update(self):
-        '''Update to show the new hue.'''
-        if not self.value_freezer.frozen and \
-           self.hue != self.hue_selection_dialog.hue:
-            self.hue = self.hue_selection_dialog.hue
-            self.spin_ctrl.SetValue(ratio_to_round_degrees(self.hue))
-    
 
-            
-    def _on_spin(self, event):
-        self.hue_selection_dialog.setter(
-            degrees_to_ratio(self.spin_ctrl.Value)
-        )
-
-        
-    def _on_text(self, event):
-        with self.value_freezer:
-            self.hue_selection_dialog.setter(
-                degrees_to_ratio(self.spin_ctrl.Value)
-            )
-
-            
-    #def _on_kill_focus(self, event):
-        #assert not self.value_freezer.frozen
-
-            
-    def set_focus_on_spin_ctrl_and_select_all(self):
-        '''
-        
-        
-        The "select all" part works only on Windows and generic `wx.SpinCtrl`
-        implementations.
-        '''
-        self.spin_ctrl.SetFocus()
-        self.spin_ctrl.SetSelection(-1, -1)
-
-
-
-class Comparer(CutePanel):
+class Comparer(wx.Panel):
     '''Shows the new hue compared to the old hue before dialog was started.'''
     def __init__(self, hue_selection_dialog):
         style = wx.TAB_TRAVERSAL | (wx.SIMPLE_BORDER if wx_tools.is_gtk
@@ -121,8 +47,6 @@ class Comparer(CutePanel):
         self.SetCursor(wx.StockCursor(wx.CURSOR_BULLSEYE))
         
         self.Bind(wx.EVT_LEFT_DOWN, self._on_mouse_left_down)
-        self.Bind(wx.EVT_SET_FOCUS, self._on_set_focus)
-        self.Bind(wx.EVT_KILL_FOCUS, self._on_kill_focus)
         self.Bind(wx.EVT_CHAR, self._on_char)
         
     
@@ -141,15 +65,6 @@ class Comparer(CutePanel):
         else:
             event.Skip()
             
-            
-    def _on_set_focus(self, event):
-        event.Skip()
-        self.Refresh()
-        
-
-    def _on_kill_focus(self, event):
-        event.Skip()
-        self.Refresh()
                 
         
 class HueSelectionDialog(wx.Dialog):
