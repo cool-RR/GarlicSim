e8e3ac4dc4c28d85bbec47d083d70628a66a7377
Ram Rachum <ram@rachum.com>
2011-08-01 23:11:09 +0200

    -

---
+++ garlicsim_lib/water_ripples/__init__.py
@@ -0,0 +1,16 @@
+'''
+water_ripples description.
+'''
+# (The above docstring should be in ReStructuredText.)
+
+from .state import State
+
+# Human-readable name of your simpack:
+name = '''water_ripples'''
+
+# Version number:
+__version__ = '0.1'
+
+# Tuple of tags that the simpack is related to, like 'game-theory',
+# 'electronics', 'climate', 'high-cpu' etc.
+tags = ()
